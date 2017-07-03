c4883186d6bd958b99e2b6d90800558e4e409ad4
Lucas Winningham <lucas.winningham@gmail.com>
2017-07-03 15:59:03 +0000

    Add hero image to about section

--- app/assets/javascripts/pages/home/_about.js
+++ app/assets/javascripts/pages/home/_about.js
@@ -1,5 +1,11 @@
 /* global $ */
 
 $( document ).on('turbolinks:load', function() {
+  var heroFill = function() {
+    $('section#about').css('height', $( window ).height());
+  };
   
+  heroFill();
+  
+  $( window ).resize(heroFill);
 });
