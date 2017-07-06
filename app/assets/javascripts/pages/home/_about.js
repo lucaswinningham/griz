c59acf4191e6fe5d0c620079b29c5ce4b1801ea4
Lucas Winningham <lucas.winningham@gmail.com>
2017-07-06 18:12:52 +0000

    Add color theme, remove background image

--- app/assets/javascripts/pages/home/_about.js
+++ app/assets/javascripts/pages/home/_about.js
@@ -2,7 +2,10 @@
 
 $( document ).on('turbolinks:load', function() {
   var heroFill = function() {
-    $('section#about').css('height', $( window ).height());
+    $('section#about').css({
+      'height': $( window ).height(),
+      'padding-top': $( window ).height() / 4,
+    });
   };
   
   heroFill();
