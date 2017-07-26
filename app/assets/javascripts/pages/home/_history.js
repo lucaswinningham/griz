ab7c8557d56f201a570a42608e381a7f20ef3378
Lucas Winningham <lucas.winningham@gmail.com>
2017-07-26 14:15:09 +0000

    Add history cards and connect to slider functionality

--- app/assets/javascripts/pages/home/_history.js
+++ app/assets/javascripts/pages/home/_history.js
@@ -2,14 +2,57 @@
 
 $( document ).on('turbolinks:load', function() {
   var history = 'section#history';
-    var track = history + ' div#track';
-    var slider = history + ' div#slider';
+    var track = history + ' div#history-track';
+      var detents = track + ' div#history-track-detents';
+        var detent = detents + ' span';
+    var slider = history + ' div#history-slider';
+    var cards = history + ' div#history-cards';
+      var card = cards + ' div.history-card';
   
   var $history = $(history);
     var $track = $(track);
     var $slider = $(slider);
+    var $detents = $(detents);
+    var $cards = $(cards);
+  
+  var content = [
+    {
+      head: '2017',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+    {
+      head: '2016',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+    {
+      head: '2015',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+    {
+      head: '2014',
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+  ];
+  
+  content.forEach(function(val, i) {
+    $detents.append('<span></span>');
+    
+    var cardHead = '<div class="history-card-head">' + val.head + '</div>';
+    var cardBody = '<div class="history-card-body">' + val.body + '</div>';
+    var card = '<div class="history-card">' + cardHead + cardBody + '</div>';
+    $cards.append(card);
+  });
+  
+  var $detent = $(detent);
+  var $card = $(card);
   
   var $responsive = $([slider].join(', '));
+  
+  var mousedown;
+  var trackTop;
+  var trackBot;
+  var pxDetentIncrement;
+  var detentInflections;
   
   var sectionFill = function() {
     $history.css({
@@ -19,10 +62,6 @@ $( document ).on('turbolinks:load', function() {
   
   sectionFill();
   
-  var mousedown;
-  var trackTop;
-  var trackBot;
-  
   var trackExtents = function() {
     trackTop = $track.offset().top;
     trackBot = $track.offset().top + $track.outerHeight();
@@ -30,9 +69,22 @@ $( document ).on('turbolinks:load', function() {
   
   trackExtents();
   
+  var positionDetents = function() {
+    pxDetentIncrement = (trackBot - trackTop) / ($detent.length + 1);
+  	detentInflections = [0.5 * pxDetentIncrement];
+    
+    $detent.each(function(i) {
+      $(this).css({top: (pxDetentIncrement * (i + 1)) + 'px'});
+      detentInflections.push(pxDetentIncrement * (i + 1.5));
+    });
+  };
+  
+  positionDetents();
+  
   $( window ).resize(function() {
     sectionFill();
     trackExtents();
+    positionDetents();
   });
   
   var trackSlider = function(userPosition) {
@@ -47,7 +99,23 @@ $( document ).on('turbolinks:load', function() {
       
       if (userPosition > trackTop && userPosition < trackBot) {
         if ((mid + diff > trackTop) && (mid + diff < trackBot)) {
-        	$slider.css({'top': userPosition - mousedown + cssTop + 'px'});
+          var newSliderPosition = userPosition - mousedown + cssTop;
+          
+        	$slider.css({top: newSliderPosition + 'px'});
+        	
+        	var relativeSliderPostion = newSliderPosition - $track.position().top;
+          
+          $card.each(function(i) {
+            if (relativeSliderPostion > detentInflections[i]) {
+              if (relativeSliderPostion < detentInflections[i + 1]) {
+                $(this).removeClass('away').addClass('focus');
+              } else {
+                $(this).removeClass('focus').addClass('away');
+              }
+            } else {
+              $(this).removeClass('focus away');
+            }
+          });
         }
       }
       
