f92cc3a174e19ddca7ba90f7ba57c9fbb07cf688
Lucas Winningham <lucas.winningham@gmail.com>
2017-07-27 13:36:06 +0000

    Alter colors and font sizes

--- app/assets/javascripts/pages/home/_contact.js
+++ app/assets/javascripts/pages/home/_contact.js
@@ -1,5 +1,19 @@
 /* global $ */
 
 $( document ).on('turbolinks:load', function() {
+  var contact = 'section#contact';
   
+  var $contact = $(contact);
+  
+  var sectionFill = function() {
+    $contact.css({
+      'height': $( window ).height(),
+    });
+  };
+  
+  sectionFill();
+  
+  $( window ).resize(function() {
+    sectionFill();
+  });
 });
--- app/assets/javascripts/pages/home/_work.js
+++ app/assets/javascripts/pages/home/_work.js
@@ -1,5 +1,19 @@
 /* global $ */
 
 $( document ).on('turbolinks:load', function() {
+  var work = 'section#work';
   
+  var $work = $(work);
+  
+  var sectionFill = function() {
+    $work.css({
+      'height': $( window ).height(),
+    });
+  };
+  
+  sectionFill();
+  
+  $( window ).resize(function() {
+    sectionFill();
+  });
 });
