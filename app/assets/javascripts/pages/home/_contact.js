35bc6a9fe10b347de10decb0090a725ed9c6fdc2
Lucas Winningham <lucas.winningham@gmail.com>
2017-09-12 16:32:28 +0000

    Simplify contact section

--- app/assets/javascripts/pages/home/_contact.js
+++ app/assets/javascripts/pages/home/_contact.js
@@ -6,33 +6,16 @@ $( document ).on('turbolinks:load', function() {
   var contact = 'section#contact';
     var container = contact + ' div#contact-container';
       var content = container + ' div#contact-content';
-        var anon = content + ' div#contact-anon';
-          var anonstatus = anon + ' div#contact-anon-status';
-        var response = content + ' input#contact-response';
         // var message = content + ' textarea#contact-message';
         var send = content + ' a.btn';
   
   var $contact = $(contact);
     var $container = $(container);
       // var $content = $(content);
-        var $anon = $(anon);
-          var $anonstatus = $(anonstatus);
-        var $response = $(response);
         // var $message = $(message);
         var $send = $(send);
   
   sectionInitialize($contact, $container);
-  
-  responsiveEvents($anon, function() {
-    if ($anon.hasClass('anonymous')) {
-      $anonstatus.html('RESPONSE');
-    } else {
-      $anonstatus.html('ANONYMOUS');
-    }
-    
-    $response.prop('disabled', function(i, v) { return !v; });
-    $anon.toggleClass('anonymous');
-  });
   
   responsiveEvents($send, function(e) {
     e.preventDefault();
