ba3e5706e18111bd5a6c2cbb07fd90d388427c19
Lucas Winningham <lucas.winningham@gmail.com>
2017-09-19 23:01:26 +0000

    Add heroku domain to production config action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,6 +95,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
+    :domain               => ENV["heroku_domain"],
     :port                 => 587,
     :user_name            => ENV['gmail_username'],
     :password             => ENV['gmail_password'],
