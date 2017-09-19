aa660565851336628a13a3d73631263737d9ac8c
Lucas Winningham <lucas.winningham@gmail.com>
2017-09-19 23:07:36 +0000

    Add perform deliveries to production action mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,4 +103,5 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
   config.action_mailer.default_url_options = { :host => ENV['heroku_domain'] } 
+  config.action_mailer.perform_deliveries = true  
 end
