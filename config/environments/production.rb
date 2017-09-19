237b42c2dfd5f836dfceeb5795a6d896b81b55cc
Lucas Winningham <lucas.winningham@gmail.com>
2017-09-19 23:04:42 +0000

    Add default url options to production action mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,11 +95,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
-    :domain               => ENV["heroku_domain"],
+    :domain               => ENV['heroku_domain'],
     :port                 => 587,
     :user_name            => ENV['gmail_username'],
     :password             => ENV['gmail_password'],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
+  config.action_mailer.default_url_options = { :host => ENV['heroku_domain'] } 
 end
